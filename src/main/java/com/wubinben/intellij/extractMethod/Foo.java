10a7b9eb114ad774214c38783defc16d74c54aa5
Ben Wu <wubin28@gmail.com>
2017-10-16 11:13:13 +0800

    finish Extract Method for an expression

--- src/main/java/com/wubinben/intellij/extractMethod/Foo.java
+++ src/main/java/com/wubinben/intellij/extractMethod/Foo.java
@@ -1,9 +1,17 @@
 package com.wubinben.intellij.extractMethod;
 
+import java.util.ArrayList;
+
 public class Foo {
     public void method() {
-        int a=1;
-        int b=2;
-        int c=a+b;
-        int d=a+c;
+        int a = 1;
+        int b = 2;
+        int c = add(a, b);
+        int d = add(a, c);
+    }
+
+    private int add(int a, int b) {
+        return a + b;
+    }
+
 }
