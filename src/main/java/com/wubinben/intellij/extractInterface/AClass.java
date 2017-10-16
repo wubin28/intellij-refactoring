d5c97de97335c7a2eb7ff7c5fd41dd8058c12518
Ben Wu <wubin28@gmail.com>
2017-10-16 11:28:50 +0800

    start Extract Interface

---
+++ src/main/java/com/wubinben/intellij/extractInterface/AClass.java
@@ -0,0 +1,13 @@
+package com.wubinben.intellij.extractInterface;
+
+public class AClass {
+    public static final double CONSTANT=3.14;
+
+    public void publicMethod() {
+        //some code here
+    }
+    public void secretMethod() {
+        //some code here
+    }
+
+}
