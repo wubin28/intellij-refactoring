0ee694f5218ac5d86a989c725d55c23da86c0aab
Ben Wu <wubin28@gmail.com>
2017-10-16 21:40:08 +0800

    finish Convert Anonymous To Inner

--- src/main/java/com/wubinben/intellij/convertAnonymousToInner/Class.java
+++ src/main/java/com/wubinben/intellij/convertAnonymousToInner/Class.java
@@ -3,10 +3,18 @@ package com.wubinben.intellij.convertAnonymousToInner;
 public class Class {
     public Interface method() {
         final int i = 0;
-        return new Interface() {
-            public int publicMethod() {
-                return i;
-            }
-        };
+        return new MyInterfaceClass(i);
+    }
+
+    private class MyInterfaceClass implements Interface {
+        private final int i;
+
+        public MyInterfaceClass(int i) {
+            this.i = i;
+        }
+
+        public int publicMethod() {
+            return i;
+        }
     }
 }
