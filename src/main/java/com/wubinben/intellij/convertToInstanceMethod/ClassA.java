dafc681e2df5820608fff458df2e4d7e5eb2391b
Ben Wu <wubin28@gmail.com>
2017-10-16 22:07:31 +0800

    finish Convert To Instance Method

--- src/main/java/com/wubinben/intellij/convertToInstanceMethod/ClassA.java
+++ src/main/java/com/wubinben/intellij/convertToInstanceMethod/ClassA.java
@@ -1,4 +1,8 @@
 package com.wubinben.intellij.convertToInstanceMethod;
 
 public class ClassA {
+    public void greatMethod(ClassB classB) {
+        System.out.println("classA = " + this);
+        System.out.println("classB = " + classB);
+    }
 }
--- src/main/java/com/wubinben/intellij/convertToInstanceMethod/MyClass.java
+++ src/main/java/com/wubinben/intellij/convertToInstanceMethod/MyClass.java
@@ -4,12 +4,7 @@ public class MyClass {
     ClassA classA = new ClassA();
     ClassB classB = new ClassB();
 
-    static public void greatMethod(ClassA classA, ClassB classB) {
-        System.out.println("classA = " + classA);
-        System.out.println("classB = " + classB);
-    }
-
     public void myMethod() {
-        MyClass.greatMethod(classA, classB);
+        classA.greatMethod(classB);
     }
 }
