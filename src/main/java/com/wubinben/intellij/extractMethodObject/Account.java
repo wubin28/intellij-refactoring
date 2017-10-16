92cf937c768a796f8f701ec50ab7443ea146d85c
Ben Wu <wubin28@gmail.com>
2017-10-16 13:14:26 +0800

    finish Extract Method Object

--- src/main/java/com/wubinben/intellij/extractMethodObject/Account.java
+++ src/main/java/com/wubinben/intellij/extractMethodObject/Account.java
@@ -1,10 +1,30 @@
 package com.wubinben.intellij.extractMethodObject;
 
 public class Account {
-    int gamma (int val1, ...) {
-        int c = 0;
-        int a = 0;
-        //some computations
+    int gamma (int val1) {
+        Calculations calculations = new Calculations().invoke();
+        int c = calculations.getC();
+        int a = calculations.getA();
         return c-2*a;
+    }
+
+    private class Calculations {
+        private int c;
+        private int a;
+
+        public int getC() {
+            return c;
+        }
+
+        public int getA() {
+            return a;
+        }
+
+        public Calculations invoke() {
+            c = 0;
+            a = 0;
+            //some computations
+            return this;
+        }
     }
 }
