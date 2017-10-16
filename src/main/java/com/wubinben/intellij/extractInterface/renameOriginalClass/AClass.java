32f627cdc6a4effc9a2866417c03e683f78aeaa7
Ben Wu <wubin28@gmail.com>
2017-10-16 11:42:18 +0800

    finish Extract Interface for Rename Original Class

--- src/main/java/com/wubinben/intellij/extractInterface/renameOriginalClass/AClass.java
+++ src/main/java/com/wubinben/intellij/extractInterface/renameOriginalClass/AClass.java
@@ -1,11 +1,7 @@
 package com.wubinben.intellij.extractInterface.renameOriginalClass;
 
-public class AClass {
-    public static final double CONSTANT=3.14;
-    public void publicMethod() {
-        //some code here
-    }
-    public void secretMethod() {
-    //some code here
-    }
+public interface AClass {
+    double CONSTANT=3.14;
+
+    void publicMethod();
 }
---
+++ src/main/java/com/wubinben/intellij/extractInterface/renameOriginalClass/FormerAClass.java
@@ -0,0 +1,10 @@
+package com.wubinben.intellij.extractInterface.renameOriginalClass;
+
+public class FormerAClass implements AClass {
+    public void publicMethod() {
+        //some code here
+    }
+    public void secretMethod() {
+    //some code here
+    }
+}
