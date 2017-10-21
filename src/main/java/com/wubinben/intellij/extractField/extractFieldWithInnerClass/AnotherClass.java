ab3eac45e113f03811161d46de65570e98338656
Ben Wu <wubin28@gmail.com>
2017-10-21 18:06:10 +0800

    start Extract Field with inner class - refactored

--- src/main/java/com/wubinben/intellij/extractField/extractFieldWithInnerClass/AnotherClass.java
+++ src/main/java/com/wubinben/intellij/extractField/extractFieldWithInnerClass/AnotherClass.java
@@ -2,4 +2,9 @@ package com.wubinben.intellij.extractField.extractFieldWithInnerClass;
 
 public class AnotherClass {
     public InnerClass innerClass;
+
+    public static class InnerClass {
+        public int i;
+        public int j;
+    }
 }
--- src/main/java/com/wubinben/intellij/extractField/extractFieldWithInnerClass/InnerClass.java
+++
@@ -1,6 +0,0 @@
-package com.wubinben.intellij.extractField.extractFieldWithInnerClass;
-
-public class InnerClass {
-    public int i;
-    public int j;
-}
