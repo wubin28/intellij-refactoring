d2135170730ab81f87d558dd3729ee5231c4dca4
Ben Wu <wubin28@gmail.com>
2017-10-21 18:18:55 +0800

    finish Extract Parameter

--- src/main/java/com/wubinben/intellij/extractParameter/HelloWorldPrinter.java
+++ src/main/java/com/wubinben/intellij/extractParameter/HelloWorldPrinter.java
@@ -2,9 +2,9 @@ package com.wubinben.intellij.extractParameter;
 
 public class HelloWorldPrinter {
     public static void print() {
-        System.out.println(generateText());
+        System.out.println(generateText("Hello, World!"));
     }
-    private static String generateText() {
-        return "Hello, World!".toUpperCase();
+    private static String generateText(String text) {
+        return text.toUpperCase();
     }
 }
