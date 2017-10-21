055b510570a066345322666389f4414b5c911235
Ben Wu <wubin28@gmail.com>
2017-10-21 23:13:01 +0800

    start Replace Inheritance with Delegation

---
+++ src/main/java/com/wubinben/intellij/replaceInheritanceWithDelegation/Class.java
@@ -0,0 +1,8 @@
+package com.wubinben.intellij.replaceInheritanceWithDelegation;
+
+public class Class extends SuperClass {
+    public int varInt;
+    public void openMethod() {
+
+    }
+}
---
+++ src/main/java/com/wubinben/intellij/replaceInheritanceWithDelegation/SuperClass.java
@@ -0,0 +1,8 @@
+package com.wubinben.intellij.replaceInheritanceWithDelegation;
+
+public abstract class SuperClass {
+    public static final int CONSTANT=0;
+    public abstract void openMethod();
+    public void secretMethod() {
+    }
+}
