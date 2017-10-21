2e3cd32ba46d1b9d44055f710add5bfba5771d54
Ben Wu <wubin28@gmail.com>
2017-10-21 23:16:43 +0800

    start Replace Inheritance with Delegation - refactored

--- src/main/java/com/wubinben/intellij/replaceInheritanceWithDelegation/Class.java
+++ src/main/java/com/wubinben/intellij/replaceInheritanceWithDelegation/Class.java
@@ -3,6 +3,6 @@ package com.wubinben.intellij.replaceInheritanceWithDelegation;
 public class Class extends SuperClass {
     public int varInt;
     public void openMethod() {
-
+        // biz logic
     }
 }
