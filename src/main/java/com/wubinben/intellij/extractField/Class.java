903e3cf98c44e9599d344b41c63f0d90f1d958d3
Ben Wu <wubin28@gmail.com>
2017-10-21 17:52:38 +0800

    finish Extract Field

--- src/main/java/com/wubinben/intellij/extractField/Class.java
+++ src/main/java/com/wubinben/intellij/extractField/Class.java
@@ -2,9 +2,12 @@ package com.wubinben.intellij.extractField;
 
 public class Class {
     AnotherClass anotherClass;
+    private int number;
+
     public void method() {
         int a = 1;
-        int b = a + anotherClass.intValue();
+        number = anotherClass.intValue();
+        int b = a + number;
         int c = b + anotherClass.intValue();
     }
 }
