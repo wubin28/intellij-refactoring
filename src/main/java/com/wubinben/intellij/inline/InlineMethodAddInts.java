2e9ca9978f806b273d5dcd2f048f267063339ae9
Ben Wu <wubin28@gmail.com>
2017-10-22 19:12:32 +0800

    finish Inline Method add ints

--- src/main/java/com/wubinben/intellij/inline/InlineMethodAddInts.java
+++ src/main/java/com/wubinben/intellij/inline/InlineMethodAddInts.java
@@ -5,11 +5,8 @@ public class InlineMethodAddInts {
     private int b;
 
     public void method() {
-        int c=add(a,b);
-        int d=add(a,c);
+        int c= a + b;
+        int d= a + c;
     }
 
-    private int add(int a, int b) {
-        return a+b;
-    }
 }
