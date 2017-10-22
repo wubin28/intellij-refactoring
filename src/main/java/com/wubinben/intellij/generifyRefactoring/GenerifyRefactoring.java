121a719c57da6972205dc430195635677ff4a15b
Ben Wu <wubin28@gmail.com>
2017-10-22 20:02:24 +0800

    start Generify Refactoring

---
+++ src/main/java/com/wubinben/intellij/generifyRefactoring/GenerifyRefactoring.java
@@ -0,0 +1,11 @@
+package com.wubinben.intellij.generifyRefactoring;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenerifyRefactoring {
+    public void method() {
+        List list = new LinkedList();
+        list.add("string");
+    }
+}
