5ba51d440680f6924cadee882679bdbd9bf441c5
Ben Wu <wubin28@gmail.com>
2017-10-22 20:00:03 +0800

    finish Inline to Lambdas

--- src/main/java/com/wubinben/intellij/inline/InlineToLambdas.java
+++ src/main/java/com/wubinben/intellij/inline/InlineToLambdas.java
@@ -1,17 +1,10 @@
 package com.wubinben.intellij.inline;
 
-import java.util.Comparator;
 import java.util.List;
 
 public class InlineToLambdas {
-    public class MyComparator implements Comparator<String> {
-        @Override
-        public int compare(String s1, String s2) {
-            return 0;
-        }
-    }
 
     void sort(List<String> scores) {
-        scores.sort(new MyComparator());
+        scores.sort((s1, s2) -> 0);
     }
 }
