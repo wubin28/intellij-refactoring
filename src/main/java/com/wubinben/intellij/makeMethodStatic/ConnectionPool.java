d5891352e1387365841f30b136b58d4db9274d21
Ben Wu <wubin28@gmail.com>
2017-10-22 17:58:17 +0800

    finish Make Method Static - refactored

--- src/main/java/com/wubinben/intellij/makeMethodStatic/ConnectionPool.java
+++ src/main/java/com/wubinben/intellij/makeMethodStatic/ConnectionPool.java
@@ -3,8 +3,13 @@ package com.wubinben.intellij.makeMethodStatic;
 public class ConnectionPool {
     public int i;
     public int j;
+
     public void getConnection() {
-        int k = i + j;
+        getConnection(this);
+    }
+
+    public static void getConnection(ConnectionPool connectionPool) {
+        int k = connectionPool.i + connectionPool.j;
         //...
     }
 }
