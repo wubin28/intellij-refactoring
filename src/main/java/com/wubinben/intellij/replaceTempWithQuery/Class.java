1d11417ad31daa4c5949053f2b795158445a7cfd
Ben Wu <wubin28@gmail.com>
2017-10-22 08:12:01 +0800

    finish Replace Temp With Query

--- src/main/java/com/wubinben/intellij/replaceTempWithQuery/Class.java
+++ src/main/java/com/wubinben/intellij/replaceTempWithQuery/Class.java
@@ -3,8 +3,11 @@ package com.wubinben.intellij.replaceTempWithQuery;
 public class Class {
     public void method() {
         String str ="str";
-        String aString = returnString().concat(str);
-        System.out.println(aString);
+        System.out.println(aString(str));
+    }
+
+    private String aString(String str) {
+        return returnString().concat(str);
     }
 
     private String returnString() {
