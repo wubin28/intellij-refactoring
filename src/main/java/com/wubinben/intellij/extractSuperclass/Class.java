f047f646cf1f5af4ca76b03b89de7aa4e145133d
Ben Wu <wubin28@gmail.com>
2017-10-22 23:06:20 +0800

    finish Extract Superclass

--- src/main/java/com/wubinben/intellij/extractSuperclass/Class.java
+++ src/main/java/com/wubinben/intellij/extractSuperclass/Class.java
@@ -1,18 +1,17 @@
 package com.wubinben.intellij.extractSuperclass;
 
 public class Class {
-    public int varInt;
-    private double varDouble;
     public static final int CONSTANT = 0;
+    private double varDouble;
+
     public void publicMethod() {
         //...
     }
-    public void hiddenMethod() {
-        //...
-    }
+
     public void setVarDouble(double var) {
         this.varDouble = var;
     }
+
     public double getVarDouble() {
         return varDouble;
     }
---
+++ src/main/java/com/wubinben/intellij/extractSuperclass/Subclass.java
@@ -0,0 +1,9 @@
+package com.wubinben.intellij.extractSuperclass;
+
+public class Subclass extends Class {
+    public int varInt;
+
+    public void hiddenMethod() {
+        //...
+    }
+}
